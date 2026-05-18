fec52e0dde82e44d13d84e1d87b63cccc01331ee
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-05-18 13:14:23 +0300

    style: remove red debug borders from layout

--- app/components/EventExplorer.tsx
+++ app/components/EventExplorer.tsx
@@ -13,16 +13,16 @@ interface EventExplorerProps {
 export const EventExplorer = ({ data, currentPage }: EventExplorerProps) => {
 
   return (
-    <div className="border-2 border-orange-600 dark bg-event-bg border-2 flex-1 flex flex-col lg:flex-row  min-h-0 px-6 lg:px-12 gap-4 pb-4">
+    <div className="dark bg-event-bg flex-1 flex flex-col lg:flex-row  min-h-0 px-6 lg:px-12 gap-4 pb-4">
 
       <div className="w-full lg:w-[260px] shrink-0 overflow-auto">
         <FilterSidebar />
       </div>
 
-      <main className="border-2 border-green-600 flex-1 min-h-0 w-full flex flex-col gap-4">
+      <main className="flex-1 min-h-0 w-full flex flex-col gap-4">
 
         
-        <div className="border-2 flex-1 h-full">
+        <div className="flex-1 h-full">
           <EventList events={data.content} />
         </div>
 
--- app/components/EventList.tsx
+++ app/components/EventList.tsx
@@ -4,7 +4,7 @@ import type { Event } from "../services/eventService";
 const EventList = ({ events }: { events: Event[] }) => {
   return (
     <>
-      <div className="border-2 border-red-600 h-full w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="h-full w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
